f39dfd815451d9d2ae2e8c625f72434a244cd300
sarath-sankar <ssankar@ecloudcontrl.com>
2022-10-13 11:01:31 +0000

    added helloyarn project

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+	    <h1>Success Test Output</h1>
+	    <p>This is the success test output</p>
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
